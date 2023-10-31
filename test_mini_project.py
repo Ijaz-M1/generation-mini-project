49af8cc7b8f276d8c24d113a88efa42b377361e9
Ijaz-M1 <ijaz9900@gmail.com>
2023-10-31 08:50:06 +0000

    added test for product and courier but receives some errors

--- test_mini_project.py
+++ test_mini_project.py
@@ -1,2 +1,21 @@
 import pytest
+from mini_project import produce_pretty_list
 
+from mini_project import product_list
+from mini_project import courier_list
+
+def test_produce_pretty_list_with_products():
+    # Arrange
+    expected_output = product_list
+    # Act
+    result = produce_pretty_list(product_list)
+    # Assert
+    assert result == expected_output
+
+def test_produce_pretty_list_with_couriers():
+    # Arrange
+    expected_output = courier_list
+    # Act
+    result = produce_pretty_list(courier_list)
+    # Assert
+    assert result == expected_output
