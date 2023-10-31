76b0a7dd9152dfd4c77f036f65dace9bf6f1377f
Ijaz-M1 <ijaz9900@gmail.com>
2023-10-31 15:36:35 +0000

    included 2 unit tests. one for products list and another for empty list

--- test_mini_project.py
+++ test_mini_project.py
@@ -1,21 +1,25 @@
-import pytest
+#import pytest
 from mini_project import produce_pretty_list
 
-from mini_project import product_list
-from mini_project import courier_list
-
 def test_produce_pretty_list_with_products():
+    mock_product_list = [
+    {"name": "Black tea", "price": "1.50"},
+    {"name": "Americano", "price": "1.80"},
+    {"name": "Strawberry smoothie", "price": "1.20"},
+    {"name": "Avacado sandwich", "price": "0.90"},
+    ]
     # Arrange
-    expected_output = product_list
+    expected = """Index: 0 Details: {'name': 'Black tea', 'price': '1.50'}\nIndex: 1 Details: {'name': 'Americano', 'price': '1.80'}\nIndex: 2 Details: {'name': 'Strawberry smoothie', 'price': '1.20'}\nIndex: 3 Details: {'name': 'Avacado sandwich', 'price': '0.90'}\n"""
     # Act
-    result = produce_pretty_list(product_list)
+    actual = produce_pretty_list(mock_product_list)
     # Assert
-    assert result == expected_output
+    assert actual == expected
 
-def test_produce_pretty_list_with_couriers():
+def test_produce_pretty_list_with_no_products():
+    mock_product_list = []
     # Arrange
-    expected_output = courier_list
+    expected = """"""
     # Act
-    result = produce_pretty_list(courier_list)
+    actual = produce_pretty_list(mock_product_list)
     # Assert
-    assert result == expected_output
+    assert actual == expected
